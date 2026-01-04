1fbd91b0139284cc246093c433482039e2b68fb6
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-01-04 22:58:29 +0000

    fix to get gatherLogs working on multicam

--- gatherLogs.sh
+++ gatherLogs.sh
@@ -1,24 +1,52 @@
 #!/bin/bash
-cd ~/RMS_data/logs
-mkdir logtmp
-cd logtmp
-
 #
 # Script to gather the logfiles and upload them for debugging & analysis
 # Copyright (C) 2018-2023 Mark McIntyre
 #
-source /home/${LOGNAME}/source/ukmon-pitools/ukmon.ini
+myself=$(readlink -f $0)
+here="$( cd "$(dirname "$myself")" >/dev/null 2>&1 ; pwd -P )"
+
+[ "$1" == "" ] && echo "usage: ./gatherlogs.sh UKxxxxx" && exit
+CAMID=$1
+
+cd $HOME
+[ -d logtmp_$CAMID ] && rm -Rf logtmp_$CAMID
+mkdir -p logtmp_$CAMID
+cd logtmp_$CAMID
+
+source $here/ukmon.ini
+
+# gather system logs
 sudo cp /var/log/kern.log .
-sudo chown ${user}:${LOGNAME} kern.log
-cp /var/log/messages ./messages.log
-cp /home/${LOGNAME}/source/RMS/.config ./${LOCATION}.config
-cp /home/${LOGNAME}/source/RMS/platepar_cmn2010.cal ./${LOCATION}.cal
-cp /home/${LOGNAME}/source/ukmon-pitools/*.key .
-cp /home/${LOGNAME}/source/ukmon-pitools/*.ini .
+[ -f /var/log/messages ] && sudo cp /var/log/messages ./messages.log; 
+[ -f /var/log/syslog ] && sudo cp /var/log/syslog ./messages.log
+sudo chown ${LOGNAME}:${LOGNAME} ./*.log
+
+# find the RMS config and log location
+if [ -d /home/${LOGNAME}/source/Stations/${CAMID} ] ; then
+    rootdir=/home/${LOGNAME}/source/Stations/${CAMID}
+else
+    rootdir=/home/${LOGNAME}/source/RMS
+fi 
+rmscfg=$rootdir/.config
+datadir=$(python -c "import configparser,os;cfg=configparser.ConfigParser();cfg.read('$rmscfg');print(cfg['Capture']['data_dir'])")
+logdir=$datadir/logs
+[ ! -d $logdir ] && logdir=~/RMS_data/logs
+
+# gather the RMS config and logs
+cp $rootdir/.config ./${CAMID}.config
+cp $rootdir/platepar_cmn2010.cal ./${CAMID}.cal
+cp $here/live.key ./${CAMID}.key
+cp $here/ukmon.ini .
+[ -f $here/cameras.ini ] && cp $here/cameras.ini .
 crontab -l > ./crontab.txt
-find  .. -maxdepth 1 -name "*.log*" -type f -mtime -5 -exec cp {} . \;
-ZIPFILE=/tmp/${LOCATION}_logs.tgz
-tar cvzf $ZIPFILE *.log* ${LOCATION}.config ${LOCATION}.cal crontab.txt *.key *.ini
+find  $logdir -maxdepth 1 -name "*.log*" -type f -mtime -5 -exec cp {} . \;
+
+# create a tarball and upload to the server
+ZIPFILE=/tmp/${CAMID}_logs.tgz
+tar cvzf $ZIPFILE *.log* ${CAMID}.config ${CAMID}.cal crontab.txt *.key *.ini
+echo waiting
+sleep 10
 sftp -i $UKMONKEY -q logupload@$UKMONHELPER << EOF
 cd logs
 progress
@@ -26,4 +54,4 @@ put $ZIPFILE
 exit
 EOF
 cd ..
-rm -Rf logtmp
+rm -Rf logtmp_$CAMID
