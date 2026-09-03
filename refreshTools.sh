4247729a1165f8f1da0f7794d13d7f544c91540e
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-09-03 16:49:21 +0100

    minor improvement in messages

--- refreshTools.sh
+++ refreshTools.sh
@@ -34,4 +34,4 @@ git fetch
 echo "testing connections"
 python $here/sendToLive.py test test
 python $here/uploadToArchive.py test
-echo "if you did not see success messages for each camera contact us for advice" 
+echo "if you did not see success messages for each camera plus one extra, contact us for advice" 
