c92338d211eb0741e4b0a405506e34e330bf256c
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-09-04 12:03:35 +0100

    bump version 2026.9.1 -> 2026.9.2

--- ukmonPostProc.py
+++ ukmonPostProc.py
@@ -23,7 +23,7 @@ from uploadToArchive import uploadToArchive, readIniFile, updateExtrascript
 ukmlog = logging.getLogger("ukmonlogger")
 ukmlog.setLevel(logging.INFO)
 
-versionid = "2026.9.1"
+versionid = "2026.9.2"
 
 
 def setupLogging(logpath, prefix):
