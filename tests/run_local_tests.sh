ebaec8b0d89a9d18db46c428edd01ac09f6e0e07
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-09-23 11:19:47 +0100

    add script to run tests locally

---
+++ tests/run_local_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# run local tests on a Pi or Multicam box
+
+# Copyright (c) Mark McIntyre
+
+here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+source ~/vRMS/bin/activate
+SRCDIR=$here/..
+export PYTHONPATH=$SRCDIR:$PYTHONPATH
+pytest
